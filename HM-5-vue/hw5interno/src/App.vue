fc53cdafe9101a7b720a5a8200dbef357e7c97d3
Воронцов Илья Викторович <Nebmes@mail.ru>
2023-11-07 00:33:58 +0300

    add homework6

---
+++ HM-5-vue/hw5interno/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+    <HeaderComp />
+    <BlogComp />
+    <FooterComp />
+    <HeaderComp />
+    <BlogDetailsComp />
+    <FooterComp />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import BlogComp from './components/BlogComp.vue'
+import HeaderComp from './components/HeaderComp.vue';
+import FooterComp from './components/FooterComp.vue';
+import BlogDetailsComp from './components/blogDetailsComp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    BlogComp,
+    BlogDetailsComp,
+    HeaderComp,
+    FooterComp
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
